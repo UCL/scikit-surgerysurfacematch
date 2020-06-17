fe1162164626cb178bf89e7abc1489631ba6cde7
Matt Clarkson <m.clarkson@ucl.ac.uk>
2020-06-17 15:18:21 +0100

    Issue #3: First pass, mosaiicing reconstruction pipeline.

--- sksurgerysurfacematch/pipelines/register_cloud_to_stereo_mosaic.py
+++ sksurgerysurfacematch/pipelines/register_cloud_to_stereo_mosaic.py
@@ -1,37 +1,41 @@
 # -*- coding: utf-8 -*-
 
-""" Pipeline to register 3D point cloud to mosaiced surface reconstruction.  """
+""" Pipeline to register 3D point cloud to mosaic'ed surface reconstruction. """
 
-import copy
 import numpy as np
 import cv2
+import sksurgeryopencvpython as cvcpp
+import sksurgerycore.algorithms.procrustes as proc
 import sksurgerysurfacematch.interfaces.video_segmentor as vs
-import sksurgerysurfacematch.interfaces.stereo_reconstructor as sr
+import sksurgerysurfacematch.algorithms.reconstructor_with_rectified_images \
+    as sr
 import sksurgerysurfacematch.interfaces.rigid_registration as rr
 
 
-# pylint:disable=too-many-instance-attributes
+# pylint:disable=too-many-instance-attributes, invalid-name, too-many-locals
+# pylint:disable=unsubscriptable-object
+
 class Register3DToMosaicedStereoVideo:
     """
     Class to register a point cloud to a series of surfaces
     derived from stereo video, and stitched together.
     """
-    def __init__(self,
-                 video_segmentor: vs.VideoSegmentor,
-                 surface_reconstructor: sr.StereoReconstructor,
-                 rigid_registration: rr.RigidRegistration,
-                 number_of_keypoints: int = 25,
-                 left_mask: np.ndarray = None,
-                 voxel_reduction: list = None,
-                 statistical_outlier_reduction: list = None
-                 ):
+    def __init__(
+            self,
+            video_segmentor: vs.VideoSegmentor,
+            surface_reconstructor: sr.StereoReconstructorWithRectifiedImages,
+            rigid_registration: rr.RigidRegistration,
+            min_number_of_keypoints: int = 25,
+            left_mask: np.ndarray = None,
+            voxel_reduction: list = None,
+            statistical_outlier_reduction: list = None):
         """
         Uses Dependency Injection for each main component.
 
         :param video_segmentor: Optional class to pre-segment the video.
         :param surface_reconstructor: Mandatory class to do reconstruction.
         :param rigid_registration: Mandatory class to perform rigid alignment.
-        :param number_of_keypoints: Number of keypoints to use for matching.
+        :param min_number_of_keypoints: Number of keypoints to use for matching.
         :param left_mask: a static mask to apply to stereo reconstruction.
         :param voxel_reduction: [vx, vy, vz] parameters for PCL
         Voxel Grid reduction.
@@ -41,24 +45,28 @@ class Register3DToMosaicedStereoVideo:
         self.video_segmentor = video_segmentor
         self.surface_reconstructor = surface_reconstructor
         self.rigid_registration = rigid_registration
-        self.number_of_keypoints = number_of_keypoints
+        self.min_number_of_keypoints = min_number_of_keypoints
         self.left_static_mask = left_mask
         self.voxel_reduction = voxel_reduction
         self.statistical_outlier_reduction = statistical_outlier_reduction
-        self.previous_image = None
-        self.previous_recon = None
         self.previous_keypoints = None
         self.previous_descriptors = None
+        self.previous_good_l2r_matches = None
+        self.previous_good_l2r_matched_descriptors = None
+        self.previous_triangulated_key_points = None
+        self.previous_recon = None
 
     def reset(self):
         """
         Reset's internal data members, so that you can start accumulating
         data again.
         """
-        self.previous_image = None
-        self.previous_recon = None
         self.previous_keypoints = None
         self.previous_descriptors = None
+        self.previous_good_l2r_matches = None
+        self.previous_good_l2r_matched_descriptors = None
+        self.previous_triangulated_key_points = None
+        self.previous_recon = None
 
     def grab(self,
              left_image: np.ndarray,
@@ -93,10 +101,52 @@ class Register3DToMosaicedStereoVideo:
 
         orb = cv2.ORB_create()
 
-        current_key_points, current_descriptors = \
+        current_left_key_points, current_left_descriptors = \
             orb.detectAndCompute(left_image, None)
 
-        if self.previous_image is None:
+        current_right_key_points, current_right_descriptors = \
+            orb.detectAndCompute(right_image, None)
+
+        index_params = dict(algorithm=6,
+                            table_number=6,
+                            key_size=12,
+                            multi_probe_level=1)  # 6=FLANN_INDEX_LSH
+        search_params = dict(checks=50)
+
+        flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+        left_to_right_matches = flann.knnMatch(
+            current_left_descriptors,
+            current_right_descriptors, k=2)
+
+        # Keep good matches, based on Lowe's ratio test.
+        good_l2r_matches = []
+        for m, n in left_to_right_matches:
+            if m.distance < 0.7 * n.distance:
+                good_l2r_matches.append(m)
+
+        if len(good_l2r_matches) > self.min_number_of_keypoints:
+
+            left_descriptors = np.zeros((len(good_l2r_matches),
+                                         len(current_left_descriptors[0])),
+                                        dtype=np.uint8)
+            for i, m in enumerate(good_l2r_matches):
+                left_descriptors[i, :] = current_left_descriptors[m.queryIdx]
+
+            left_pts = np.float32([current_left_key_points[m.queryIdx].pt
+                                   for m in good_l2r_matches])
+            right_pts = np.float32([current_right_key_points[m.trainIdx].pt
+                                    for m in good_l2r_matches])
+            paired_pts = np.zeros((left_pts.shape[0], 4))
+            paired_pts[:, 0:2] = left_pts
+            paired_pts[:, 2:4] = right_pts
+            triangulated_l2r_key_points = \
+                cvcpp.triangulate_points_using_hartley(paired_pts,
+                                                       left_camera_matrix,
+                                                       right_camera_matrix,
+                                                       left_to_right_rmat,
+                                                       left_to_right_tvec)
+
             full_reconstruction = \
                 self.surface_reconstructor.reconstruct(left_image,
                                                        left_camera_matrix,
@@ -108,15 +158,57 @@ class Register3DToMosaicedStereoVideo:
                                                        left_to_right_tvec,
                                                        left_mask
                                                        )
-            self.previous_image = copy.deepcopy(left_image)
+            full_reconstruction = full_reconstruction[:, 0:3]
+
+            # Match to previous frame
+            if self.previous_keypoints is not None and \
+                    self.previous_descriptors is not None:
+
+                previous_to_current = flann.knnMatch(
+                    self.previous_good_l2r_matched_descriptors,
+                    left_descriptors,
+                    k=2)
+
+                # Keep good matches, based on Lowe's ratio test.
+                good_prev_to_current_matches = []
+                for i, id_pair in enumerate(previous_to_current):
+                    if id_pair is not None and len(id_pair) == 2:
+                        m, n = id_pair
+                        if m.distance < 0.7 * n.distance:
+                            good_prev_to_current_matches.append(m)
+
+                if len(good_prev_to_current_matches) > \
+                        self.min_number_of_keypoints:
+
+                    prev_3d_pts = np.float32(
+                        [self.previous_triangulated_key_points[m.queryIdx]
+                         for m in good_prev_to_current_matches])
+                    current_3d_pts = np.float32(
+                        [triangulated_l2r_key_points[m.trainIdx]
+                         for m in good_prev_to_current_matches])
+
+                    # Compute rigid body transform. Maybe use RANSAC?
+                    rmat, tvec, fre = proc.orthogonal_procrustes(current_3d_pts,
+                                                                 prev_3d_pts)
+
+                    if fre < 1:
+                        # Transform previous point cloud to current
+                        transformed_point_cloud = \
+                            np.transpose(np.matmul(rmat,
+                                                   np.transpose(
+                                                       self.previous_recon))
+                                         + tvec)
+
+                        # Combine and simplify?
+                        full_reconstruction = \
+                            np.vstack((transformed_point_cloud,
+                                       full_reconstruction))
+
+            # Save current iteration, such that next iteration it will
+            # be called the 'previous' iteration, for tracking purposes.
+            self.previous_keypoints = current_left_key_points
+            self.previous_descriptors = current_left_descriptors
+            self.previous_good_l2r_matches = good_l2r_matches
+            self.previous_good_l2r_matched_descriptors = left_descriptors
+            self.previous_triangulated_key_points = triangulated_l2r_key_points
             self.previous_recon = full_reconstruction
-            self.previous_keypoints = current_key_points
-            self.previous_descriptors = current_descriptors
-
-
-        # Match
-        b_f = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-        matches = b_f.match(self.previous_descriptors, current_descriptors)
-        matches = sorted(matches, key=lambda x: x.distance)
-        best_matches = matches[:self.number_of_keypoints]
-        print(best_matches)
---
+++ tests/test_mosaic_clouds.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+import cv2
+import numpy as np
+
+import sksurgerysurfacematch.utils.projection_utils as pu
+import sksurgerysurfacematch.algorithms.sgbm_reconstructor as sr
+import sksurgerysurfacematch.pipelines.register_cloud_to_stereo_mosaic as sm
+
+
+def test_point_cloud_mosaiicing():
+
+    left_intrinsics = np.loadtxt('tests/data/open_cas_tmi/Stereo_SD_d_complete_22/calib.left.intrinsics.txt')
+    right_intrinsics = np.loadtxt('tests/data/open_cas_tmi/Stereo_SD_d_complete_22/calib.right.intrinsics.txt')
+    left_distortion = np.loadtxt('tests/data/open_cas_tmi/Stereo_SD_d_complete_22/calib.left.distortion.txt')
+    right_distortion = np.loadtxt('tests/data/open_cas_tmi/Stereo_SD_d_complete_22/calib.right.distortion.txt')
+    l2r_matrix = np.loadtxt('tests/data/open_cas_tmi/Stereo_SD_d_complete_22/calib.l2r.txt')
+
+    l2r_rmat = l2r_matrix[0:3, 0:3]
+    l2r_tvec = l2r_matrix[0:3, 3:4]
+
+    left_image_t0 = cv2.imread('tests/data/open_cas_tmi/Stereo_SD_d_complete_22/Stereo_SD_d_complete_22_IMG_left.bmp')
+    left_undistorted_t0 = cv2.undistort(left_image_t0, left_intrinsics, left_distortion)
+    left_mask_t0 = cv2.imread('tests/data/open_cas_tmi/Stereo_SD_d_complete_22/Stereo_SD_d_complete_22_MASK_eval_inverted.png')
+    left_mask_t0 = cv2.cvtColor(left_mask_t0, cv2.COLOR_BGR2GRAY)
+    right_image_t0 = cv2.imread('tests/data/open_cas_tmi/Stereo_SD_d_complete_22/Stereo_SD_d_complete_22_IMG_right.bmp')
+    right_undistorted_t0 = cv2.undistort(right_image_t0, right_intrinsics, right_distortion)
+
+    video_mosaiicer = sm.Register3DToMosaicedStereoVideo(None,
+                                                         sr.SGBMReconstructor(),
+                                                         None,
+                                                         left_mask=left_mask_t0)
+
+    video_mosaiicer.grab(left_undistorted_t0,
+                         left_intrinsics,
+                         left_distortion,
+                         right_undistorted_t0,
+                         right_intrinsics,
+                         right_distortion,
+                         l2r_rmat,
+                         l2r_tvec)
+
+    video_mosaiicer.grab(left_undistorted_t0,
+                         left_intrinsics,
+                         left_distortion,
+                         right_undistorted_t0,
+                         right_intrinsics,
+                         right_distortion,
+                         l2r_rmat,
+                         l2r_tvec)
+
+    assert True
