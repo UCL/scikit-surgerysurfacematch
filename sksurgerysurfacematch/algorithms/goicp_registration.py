f3fede6fc96622179d57b8f68490f60e73443b17
Thomas Dowrick <tdowrick@gmail.com>
2020-08-11 13:57:33 +0100

    Issue #5: Add GoICPRegistration class

---
+++ sksurgerysurfacematch/algorithms/goicp_registration.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+
+""" PCL ICP implementation of RigidRegistration interface. """
+#pylint:disable=invalid-name, no-name-in-module
+
+import numpy as np
+from sksurgerygoicppython import  GoICP, POINT3D
+import sksurgerysurfacematch.interfaces.rigid_registration as rr
+
+def numpy_to_POINT3D_array(numpy_pointcloud):
+    """ Covert numpy array to POINT3D array suitable for GoICP algorithm."""
+    plist = numpy_pointcloud.tolist()
+    p3dlist = []
+    for x, y, z in plist:
+        pt = POINT3D(x, y, z)
+        p3dlist.append(pt)
+    return numpy_pointcloud.shape[0], p3dlist
+
+class GoICPRegistration(rr.RigidRegistration):
+    """
+    Class that uses GoICP implementation to register fixed/moving clouds.
+    At the moment, we are just relying on all default parameters.
+    :param dt_size: GoICP distance transform size
+    :param dt_factor: GoICP distance transform factor
+    """
+
+    def __init__(self, dt_size=300, dt_factor=2.0):
+
+        self.goicp = GoICP()
+        self.goicp.setDTSizeAndFactor(dt_size, dt_factor)
+
+    def register(self,
+                 fixed_cloud: np.ndarray,
+                 moving_cloud: np.ndarray,
+                 ):
+        """
+        Uses GoICP library, wrapped in scikit-surgerygoicp.
+
+        :param fixed_cloud: [Nx3] fixed point cloud.
+        :param moving_cloud: [Mx3] moving point cloud.
+        :return: [4x4] transformation matrix, moving-to-fixed space.
+        """
+
+        Nm, a_points = numpy_to_POINT3D_array(moving_cloud)
+        Nd, b_points = numpy_to_POINT3D_array(fixed_cloud)
+        self.goicp.loadModelAndData(Nm, a_points, Nd, b_points)
+        self.goicp.BuildDT()
+        residual = self.goicp.Register()
+
+        opt_rot = self.goicp.optimalRotation()
+        opt_trans = self.goicp.optimalTranslation()
+
+        rigid_transformation = np.identity(4)
+
+        for i in range(3):
+            for j in range(3):
+                rigid_transformation[i][j] = opt_rot[i][j]
+
+        rigid_transformation[0][3] = opt_trans[0]
+        rigid_transformation[1][3] = opt_trans[1]
+        rigid_transformation[2][3] = opt_trans[2]
+
+        return residual, rigid_transformation
--- tests/test_register_point_cloud.py
+++ tests/test_register_point_cloud.py
@@ -6,6 +6,7 @@ import numpy as np
 import sksurgerysurfacematch.utils.projection_utils as pu
 import sksurgerysurfacematch.algorithms.sgbm_reconstructor as sr
 import sksurgerysurfacematch.algorithms.pcl_icp_registration as pir
+import sksurgerysurfacematch.algorithms.goicp_registration as goicp
 import sksurgerysurfacematch.pipelines.register_cloud_to_stereo_reconstruction as reg
 
 
@@ -69,3 +70,63 @@ def test_point_cloud_registration():
 
     assert residual < 5.0
 
+
+def test_point_cloud_registration_goicp():
+
+    left_intrinsics = np.loadtxt('tests/data/open_cas_tmi/Stereo_SD_d_complete_22/calib.left.intrinsics.txt')
+    right_intrinsics = np.loadtxt('tests/data/open_cas_tmi/Stereo_SD_d_complete_22/calib.right.intrinsics.txt')
+    left_distortion = np.loadtxt('tests/data/open_cas_tmi/Stereo_SD_d_complete_22/calib.left.distortion.txt')
+    right_distortion = np.loadtxt('tests/data/open_cas_tmi/Stereo_SD_d_complete_22/calib.right.distortion.txt')
+    l2r_matrix = np.loadtxt('tests/data/open_cas_tmi/Stereo_SD_d_complete_22/calib.l2r.txt')
+
+    l2r_rmat = l2r_matrix[0:3, 0:3]
+    l2r_tvec = l2r_matrix[0:3, 3:4]
+
+    pointcloud_file = 'tests/data/open_cas_tmi/Stereo_SD_d_complete_22/Stereo_SD_d_complete_22_CT_cut.xyz'
+    point_cloud = np.loadtxt(pointcloud_file)
+
+    model_to_camera = np.eye(4)
+
+    left_image = cv2.imread('tests/data/open_cas_tmi/Stereo_SD_d_complete_22/Stereo_SD_d_complete_22_IMG_left.bmp')
+    left_undistorted = cv2.undistort(left_image, left_intrinsics, left_distortion)
+    left_mask = cv2.imread('tests/data/open_cas_tmi/Stereo_SD_d_complete_22/Stereo_SD_d_complete_22_MASK_eval_inverted.png')
+    left_mask = cv2.cvtColor(left_mask, cv2.COLOR_BGR2GRAY)
+    right_image = cv2.imread('tests/data/open_cas_tmi/Stereo_SD_d_complete_22/Stereo_SD_d_complete_22_IMG_right.bmp')
+    right_undistorted = cv2.undistort(right_image, right_intrinsics, right_distortion)
+
+    # Produce picture of gold standard registration.
+    pu.reproject_and_save(left_undistorted, model_to_camera, point_cloud, left_intrinsics,
+                          output_file='tests/output/open_cas_tmi_gold.png')
+
+    # Create registration pipeline.
+    reg_points_to_vid = \
+        reg.Register3DToStereoVideo(None,
+                                    sr.SGBMReconstructor(),
+                                    goicp.GoICPRegistration(),
+                                    left_intrinsics,
+                                    left_distortion,
+                                    right_intrinsics,
+                                    right_distortion,
+                                    l2r_rmat,
+                                    l2r_tvec,
+                                    left_mask=left_mask,
+                                    z_range=[45, 65],
+                                    voxel_reduction=[5, 5, 5]
+                                    )
+
+    residual, registration = reg_points_to_vid.register(point_cloud,
+                                                        left_undistorted,
+                                                        right_undistorted,
+                                                        model_to_camera
+                                                        )
+
+    print(f'Model: {pointcloud_file}')
+    print(f'{len(point_cloud)} points in reference point cloud')
+
+    print("Residual:" + str(residual))
+    print("Registration, using single-view, with full point cloud:\n" + str(registration))
+
+    pu.reproject_and_save(left_undistorted, registration, point_cloud, left_intrinsics,
+                          output_file='tests/output/open_cas_tmi_registered.png')
+
+    assert residual < 5.0
